46ec09d4e166d6e0f08d9d01e064f0bdfcacba89
Elliott Moore <elliott-moore@outlook.com>
2017-05-13 13:37:41 +1000

    creating base app

---
+++ src/tandaApp/module.js
@@ -0,0 +1,49 @@
+define([
+  'angular',
+  'angular-ui-router',
+  'angular-resource',
+  'oclazyload',
+  'angular-bootstrap'
+], (angular) => {
+  const tandaApp = angular.module('tandaApp', [
+    'ui.router',
+    'ngResource',
+    'oc.lazyLoad',
+    'ui.bootstrap',
+    'tandaSDK',
+    'punctualWidget'
+  ]);
+  
+ tandaApp
+  .config((
+    $stateProvider,
+    $urlRouterProvider,
+    $ocLazyLoadProvider
+  ) => {
+    $stateProvider
+    .state('app', {
+      abstract: true,
+      url: '/',
+      views: {
+        app: {
+          templateUrl: './tandaApp/views/index.html'
+        },
+        'navbar@app': {
+          templateUrl: './tandaApp/templates/nav.tpl.html',
+          controller: 'navCtrl'
+        }
+      },
+      resolve: {
+        deps: ($ocLazyLoad) => {
+          $ocLazyLoad.load([
+            './tandaApp/controllers/nav.ctrl.js'
+          ])
+        }
+      }
+    });
+
+    $urlRouterProvider.otherwise('/');
+  });
+
+  return tandaApp;
+});
