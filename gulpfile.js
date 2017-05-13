7cd06e352189b6b05135f7f1399e8b66ed5c9736
Elliott Moore <elliott-moore@outlook.com>
2017-05-13 16:24:48 +1000

    created gulp process

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const path = require('path');
+const del = require('del');
+
+const config = {
+  src: {
+    js: ['./src/**/*.js', '!node_modules/**/*.js'],
+    html: './src/**/*.html',
+    css: './src/**/*.css',
+    assets: ['./src/assets/**/*.png', './src/assets/**/*.jpg']
+  },
+  dest: './'
+};
+
+const sources = Object.keys(config.src).map(k => config.src[k]);
+
+gulp.task('clean', () => del([
+  'dist/**/*',
+  'assets/**/*',
+  '*.html'
+]));
+
+gulp.task('babel', ['clean'], () => {
+  gulp.src(config.src.js)
+  .pipe(babel())
+  .pipe(gulp.dest(path.join(config.dest, 'dist')));
+});
+
+gulp.task('copy', ['clean'], () => {
+  gulp.src(config.src.html)
+  .pipe(gulp.dest(path.join(config.dest, 'dist')));
+});
+
+gulp.task('assets', ['clean'], () => {
+  gulp.src(config.src.assets)
+  .pipe(gulp.dest(path.join(config.dest, 'dist')));
+});
+
+gulp.task('css', ['clean'], () => {
+  gulp.src(config.src.css)
+  .pipe(gulp.dest(path.join(config.dest, 'dist')));
+});
+
+gulp.task('build', ['babel', 'copy', 'assets', 'css']);
+gulp.task('default', ['build']);
